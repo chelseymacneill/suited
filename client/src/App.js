3c8a45a4b252751635289a7452a7f547e862a508
PTMcEneaney <pagemceneaney@gmail.com>
2019-08-17 13:03:40 -0700

    added landing, search, and profile pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Landing from "./pages/Landing";
+import Profile from "./pages/Profile";
+
 
 function App() {
   return (
@@ -11,13 +15,30 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/saved" component={Saved} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/profile/:id" component={Profile} />
           <Route component={NoMatch} />
         </Switch>
       </div>
     </Router>
   );
 }
+
+//BASE FILE STRUCTURE
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/saved" component={Saved} />
+//           <Route component={NoMatch} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
 
 export default App;
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+
+
+class Profile extends Component {
+    state = {
+        userID: this.props.match.params.id
+    };
+
+    // componentDidMount() {
+        //API call to get user data
+    //   };
+
+    render(){
+        return(
+            <Container fluid>
+            <Row>
+              <Col size="md-12">
+                <Jumbotron>
+                  <h1>
+                      Hello World: {this.state.userID}
+                  </h1>
+                    {/* insert recommended job container and job card components */}
+                    <h2>Recommended Jobs (Job Cards) live here - from Swing Table DB Collection</h2>
+                </Jumbotron>
+              </Col>
+            </Row>
+            <Row>
+                <Col size="md-10 md-offset-1">
+                </Col>
+            </Row>
+            <Row>
+                <Col size="md-4">
+                    {/* insert user card component */}
+                    <h2>User Profile Details</h2>
+                </Col>
+                <Col size="md-8">
+                    {/* insert job container and job card components */}
+                    <h2>Job Cards live here - from Swing Table DB Collection</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col size="md-10 md-offset-1">
+                    {/* insert footer component */}
+                    <h2>Footer Down at the bottom</h2>
+                </Col>
+            </Row>
+
+            </Container>
+        )
+    }
+
+};
+
+export default Profile;
