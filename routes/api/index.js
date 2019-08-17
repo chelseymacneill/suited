fd9ed757ccc1d5e63351e5c709b6292a1ba6e849
PTMcEneaney <pagemceneaney@gmail.com>
2019-08-17 13:45:53 -0700

    adding passport docs, just before creating routes/api/users.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,12 +2,16 @@ const path = require("path");
 const router = require("express").Router();
 const bookRoutes = require("./books");
 const googleRoutes = require("./google");
+const usersRoutes = require("./users")
 
 // Book routes
 router.use("/books", bookRoutes);
 
 // Google Routes
 router.use("/google", googleRoutes);
+
+//Users Routes
+router.use("/users", usersRoutes)
 
 // For anything else, render the html page
 router.use(function(req, res) {
--- server.js
+++ server.js
@@ -2,30 +2,93 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
+//Initiate our app
 const app = express();
+
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+
+
 const PORT = process.env.PORT || 3001;
 
-// Configure body parsing for AJAX requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'suited_app', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+  app.use(errorHandler());
 }
 
-// Add routes, both API and view
-app.use(routes);
+//Configure Mongoose
+mongoose.connect('mongodb://localhost/suited_app');
+mongoose.set('debug', true);
 
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
+//models & Routes
+require('./models/users');
+require('./config/passport');
 
-// Start the API server
-app.listen(PORT, () =>
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-);
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+app.listen(8001, () => console.log('Server running on http://localhost:8001/'));
+//////////////////////////////////////////////////////////////////////////////////
+
+// // Configure body parsing for AJAX requests
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+// // Add routes, both API and view
+// app.use(routes);
+
+// // Connect to the Mongo DB
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+//   {
+//     useCreateIndex: true,
+//     useNewUrlParser: true
+//   }
+// );
+
+// // Start the API server
+// app.listen(PORT, () =>
+//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+// );
