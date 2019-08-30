8a86e1467cd012341a2062c3817bf77cf0b96dbd
PTMcEneaney <pagemceneaney@gmail.com>
2019-08-29 20:08:19 -0700

    changed server path - line 40

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.use(cors());
 app.use(require("morgan")("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(session({ secret: 'mongod-vs-nodemon', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 app.use( (req, res, next) => {
   console.log('req.session', req.session);
