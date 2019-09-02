41e813238ce052b1d4e76cfba16bc01130fedd84
cmac <chelsey.macneill@gmail.com>
2019-09-01 22:43:23 -0700

    updated data model for quiz

--- models/quiz.js
+++ models/quiz.js
@@ -2,19 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const quizSchema = new Schema({
-  //CM : Commented out ObjectID because it's undefined and causinsg the app to crash
-  //id: { type: ObjectID, unique: true },
-  // Do we need them to input thier name if it should already be associated with thier userId
-  firstName: { type: String },
-  lastName: { type: String },
-  city: { type: String },
-  state: { type: String },
-  zipCode: { type: String },
-  // Need to pass which question it is to the results
-  QuestionID: { type: Array },
-  yes: { type: Array },
-  willing: { type: Array },
-  no: { type: Array }
+  username: { type: String },
+  javaScriptSkill: { type: Boolean },
+  pythonSkill: { type: Boolean },
+  javaSkill: { type: Boolean },
+  cSharpSkill: { type: Boolean },
+  sqlSkill: { type: Boolean }
 });
 
 const Quiz = mongoose.model("Quiz", quizSchema);
