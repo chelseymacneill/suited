e771e923dfb6d89bf1d840e1c071fbe69611a24f
PTMcEneaney <pagemceneaney@gmail.com>
2019-09-02 18:20:48 -0700

    Revert "Revert "hot-fix after conflict resolution""
    This reverts commit 7caa854614079cea0affaac93147a54023812773.

---
+++ client/src/components/ModalProfile/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+class ModalProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
+   
+
+  render() {
+    return (
+      <div>
+        <Button color="success" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>{this.props.modalTitle}</ModalHeader>
+          <ModalBody>
+            {this.props.children}
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ModalProfile;
