43537807023dba2327093271a171098655e74737
PTMcEneaney <pagemceneaney@gmail.com>
2019-09-02 21:39:34 -0700

    package.json

--- client/src/components/ModalProfile/index.js
+++ client/src/components/ModalProfile/index.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+// import React from 'react';
+// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-class ModalProfile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    };
+// class ModalProfile extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       modal: false
+//     };
 
-    this.toggle = this.toggle.bind(this);
-  }
+//     this.toggle = this.toggle.bind(this);
+//   }
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
+//   toggle() {
+//     this.setState(prevState => ({
+//       modal: !prevState.modal
+//     }));
+//   }
 
    
 
-  render() {
-    return (
-      <div>
-        <Button color="success" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>{this.props.modalTitle}</ModalHeader>
-          <ModalBody>
-            {this.props.children}
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <Button color="success" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+//         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+//           <ModalHeader toggle={this.toggle}>{this.props.modalTitle}</ModalHeader>
+//           <ModalBody>
+//             {this.props.children}
+//           </ModalBody>
+//           <ModalFooter>
+//             <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+//             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+//           </ModalFooter>
+//         </Modal>
+//       </div>
+//     );
+//   }
+// }
 
-export default ModalProfile;
+// export default ModalProfile;
