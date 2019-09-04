43700ddd52dc745c9cd7eff137530d19abcdf5b3
SamanthaAO <samanthaaorcutt@gmail.com>
2019-09-04 16:54:52 -0700

    fixed 500 response that was crashign search if job postings had been taken down with a splice

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -24,9 +24,9 @@ module.exports = {
                 // Collect all promises until we have them all
                 Promise.all(
                     // Use map because it returns an array (of promises in our case)
-                    jsonJobs.map(job => {
+                    jsonJobs.map((job, i) => {
                         return axios.get(job.url).then(function (response) {
-                            if (response != undefined && response.status != 502) {
+                            // if (response != undefined && response.status != 500) {
                                 const $ = cheerio.load(response.data);
                                 const str = $("body").text().toLowerCase();
                                 const green = goodArray.filter(element => str.includes(element));
@@ -37,16 +37,20 @@ module.exports = {
                                 job.red = red;
                                 console.log(job);
 
-                            }
-                            else {
-                                console.log("skipped due to error")
-                            }
+                            // }
+                            // else {
+                            //     console.log("skipped due to error")
+                            // }
+                        }).catch((err) => {
+                            console.log("ERROR ERROR ERROR!!!!!" +err)
+                            jsonJobs.splice(i, 1);
                         });
                     })
                     
                 ).then(function () {
                     
                             res.json(jsonJobs);
+                            console.log(jsonJobs);
                             console.log("jobs displayed")
                         
                 }).catch(err => console.log(err));
