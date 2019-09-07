15b7ec7695ca5e7b45b84b8e0e2d4df3267b9a05
PTMcEneaney <pagemceneaney@gmail.com>
2019-09-06 17:10:23 -0700

    cleaned up some commments, removed unused components, added jumbotron image

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,36 +2,36 @@ import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 // import API from "../utils/API";
 // import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
+import { Col, Row, Container } from "reactstrap";
+import Footer from "../components/Footer";
+
 
 class Landing extends Component {
     state = {};
 
     render(){
         return(
-            <Container fluid>
+            <Container>
             <Row>
               <Col size="md-12">
                 <Jumbotron>
-                  <h1>
-                      Hello World: Search Bar Here
+                  <h1 className="p-2 text-center">
+                      Find positions that are suited to you
+                      {/* & <br /> learn what skills you need to be <br />a competitive applicant. */}
                   </h1>
-                  <input className="form-control" type="text" placeholder="Default input"></input>
-
                 </Jumbotron>
               </Col>
             </Row>
             <Row>
-                <Col size="md-10 md-offset-1">
+                <Col size="12">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </Col>
             </Row>
             <Row>
-                <Col size="md-10 md-offset-1">
-                    {/* insert footer component */}
-                    <h2>Footer Down at the bottom</h2>
+                <Col size="12">
+                    <Footer />
                 </Col>
             </Row>
             </Container>
